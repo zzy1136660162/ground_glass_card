7a52104e5d107426c143c46a6d509182459d0199
张子扬 <oncwnuAFB_twnFPgWlNJAqXLJeHQ@git.weixin.qq.com>
2024-01-27 10:18:00 +0800

    init

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,12 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(home: GlassCard()));
 
+const TextStyle smallTextStyle = TextStyle(
+    fontSize: 10,
+    fontWeight: FontWeight.w400,
+    color: Color.fromRGBO(0, 0, 0, 0.6));
+
+
 class GlassCard extends StatelessWidget {
   const GlassCard({super.key});
 
@@ -19,9 +25,23 @@ class GlassCard extends StatelessWidget {
                 Container(
                   height: 200,
                   decoration: BoxDecoration(
-                    border: Border.all(color: Colors.white!, width: 0.5),
+                    // border: Border.all(color: Colors.white!, width: 0.5),
                     borderRadius: BorderRadius.circular(20),
                     color: Color(0x00ADD6EB),
+                    boxShadow:   [
+                      // 高亮色
+                      BoxShadow(
+                        color: Color(0xFFDEF1FF).withOpacity(0.5),
+                        offset: Offset(5.0, 5.0),
+                        blurRadius: 2.0,
+                      ),
+                      // 阴影色
+                      BoxShadow(
+                        color: Color(0xFF79BEE8).withOpacity(.5),
+                        offset: Offset(-5.0, -5.0),
+                        blurRadius: 2.0,
+                      ),
+                    ],
                   ),
                   child: Stack(
                     children: [
@@ -53,33 +73,138 @@ class GlassCard extends StatelessWidget {
                         child: BackdropFilter(
                           filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
                           child: Container(
-                            padding: EdgeInsets.all(30),
-                            child: Column(
+                            padding: EdgeInsets.all(25),
+                            child: Stack(
                               children: [
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                Positioned(
+                                    right: 0,
+                                    bottom: 0,
+                                    child: ColorFiltered(
+                                      colorFilter: ColorFilter.mode(
+                                        Colors.black.withOpacity(0.5),
+                                        // 调整这个值来改变暗度
+                                        BlendMode
+                                            .modulate, // 使用混合模式darken
+                                      ),
+                                      child: Image.asset(
+                                        'assets/card_images/Circuit.png',
+                                        width: 35,
+                                        color: Color.fromRGBO(
+                                            0, 0, 0, 0.5),
+                                      ),
+                                    )),
+                                Column(
+                                  mainAxisAlignment:
+                                  MainAxisAlignment.spaceAround,
+                                  crossAxisAlignment:
+                                  CrossAxisAlignment.start,
                                   children: [
-                                    Image.asset(
-                                      'assets/card_images/Brand.png',
-                                      width: 30,
+                                    Row(
+                                      mainAxisAlignment:
+                                      MainAxisAlignment
+                                          .spaceBetween,
+                                      children: [
+                                        Image.asset(
+                                          'assets/card_images/Brand.png',
+                                          width: 26,
+                                        ),
+                                        Row(
+                                          children: [
+                                            Image.asset(
+                                              'assets/card_images/icon-contactless.png',
+                                              width: 9,
+                                            ),
+                                            SizedBox(
+                                              width: 30,
+                                            ),
+                                            Image.asset(
+                                              'assets/card_images/mastercard.png',
+                                              width: 30,
+                                            ),
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                    SizedBox(
+                                      height: 20,
+                                    ),
+                                    Text(
+                                      'Credit card number',
+                                      style: smallTextStyle,
+                                    ),
+                                    Text(
+                                      '5737 4277 4925 2858',
+                                      style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight:
+                                          FontWeight.w700,
+                                          color: Color.fromRGBO(
+                                              0, 0, 0, 0.6)),
+                                    ),
+                                    SizedBox(
+                                      height: 0,
                                     ),
                                     Row(
                                       children: [
-                                        Image.asset(
-                                          'assets/card_images/icon-contactless.png',
-                                          width: 16,
-                                        ),
-                                        SizedBox(
-                                          width: 30,
-                                        ),
-                                        Image.asset(
-                                          'assets/card_images/mastercard.png',
-                                          width: 30,
-                                        ),
+                                        Expanded(
+                                            flex: 4,
+                                            child: Column(
+                                              crossAxisAlignment:
+                                              CrossAxisAlignment
+                                                  .start,
+                                              children: [
+                                                Text(
+                                                  'Name',
+                                                  style:
+                                                  smallTextStyle,
+                                                ),
+                                                Text(
+                                                    'Alexander K. Smithers',
+                                                    style: TextStyle(
+                                                        fontSize:
+                                                        12,
+                                                        fontWeight:
+                                                        FontWeight
+                                                            .w500,
+                                                        color: Color
+                                                            .fromRGBO(
+                                                            0,
+                                                            0,
+                                                            0,
+                                                            0.6)))
+                                              ],
+                                            )),
+                                        Expanded(
+                                            flex: 3,
+                                            child: Column(
+                                              crossAxisAlignment:
+                                              CrossAxisAlignment
+                                                  .start,
+                                              children: [
+                                                Text(
+                                                  'Exp. Date',
+                                                  style:
+                                                  smallTextStyle,
+                                                ),
+                                                Text('05/25',
+                                                    style: TextStyle(
+                                                        fontSize:
+                                                        16,
+                                                        fontWeight:
+                                                        FontWeight
+                                                            .w700,
+                                                        color: Color
+                                                            .fromRGBO(
+                                                            0,
+                                                            0,
+                                                            0,
+                                                            0.6))),
+                                              ],
+                                            )),
                                       ],
                                     )
                                   ],
-                                )
+                                ),
                               ],
                             ),
                           ),
